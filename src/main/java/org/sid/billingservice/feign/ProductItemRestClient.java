d7aece4929f1d5c8f708134795a64c9e736aec32
Mohamed Fquihi <mo.fquihi@gmail.com>
2022-11-10 16:54:34 +0100

    second commit

--- src/main/java/org/sid/billingservice/feign/ProductItemRestClient.java
+++ src/main/java/org/sid/billingservice/feign/ProductItemRestClient.java
@@ -4,6 +4,7 @@ import org.sid.billingservice.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.ws.rs.QueryParam;
 
@@ -11,5 +12,8 @@ import javax.ws.rs.QueryParam;
 public interface ProductItemRestClient {
     @GetMapping(path = "/products")
 
-    PagedModel<Product> pageProducts(@QueryParam(value="page") int page,int size);
+    PagedModel<Product> pageProducts(@QueryParam(value="page") int page,
+                                     @QueryParam(value="size") int size);
+    @GetMapping(path = "/products/{id}")
+    Product getProductById(@PathVariable Long id);
 }
